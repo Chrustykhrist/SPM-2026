a16b7cd4601670839c7c575d4d45f6321b11bd09
Lord-Foog1 <olivernordlander10@hotmail.com>
2026-04-13 16:46:59 +0200

    Character Movement complete

--- SPM-2026-whitebox/MyProject/Source/MyProject/PlayerCharacter.cpp
+++ SPM-2026-whitebox/MyProject/Source/MyProject/PlayerCharacter.cpp
@@ -2,6 +2,11 @@
 
 
 #include "PlayerCharacter.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
@@ -16,19 +21,97 @@ void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (ULocalPlayer* LocalPlayer = PC->GetLocalPlayer())
+		{
+			UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+			if (Subsystem && InputMappingCcntext)
+			{
+				Subsystem->AddMappingContext(InputMappingCcntext, 0);
+			}
+		}
+	}
+	
 }
 
 // Called every frame
 void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
+	if (!bRunning && AsthmaLimit < 5)
+	{
+		AsthmaLimit += GetWorld()->GetDeltaSeconds();
+	}
+	
 }
 
 // Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	if (UEnhancedInputComponent* UEnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		UEnhancedInput->BindAction(IAMove, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+		UEnhancedInput->BindAction(IALook, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+		UEnhancedInput->BindAction(IALookMouse, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+		UEnhancedInput->BindAction(IAJump, ETriggerEvent::Triggered, this, &APlayerCharacter::Jump);
+		UEnhancedInput->BindAction(IACrouch, ETriggerEvent::Triggered, this, &APlayerCharacter::PlayerCrouch);
+		UEnhancedInput->BindAction(IACrouch, ETriggerEvent::Completed, this, &APlayerCharacter::PlayerUnCrouch);
+		UEnhancedInput->BindAction(IASprint, ETriggerEvent::Triggered, this, &APlayerCharacter::Sprint);
+		UEnhancedInput->BindAction(IASprint, ETriggerEvent::Completed, this, &APlayerCharacter::SlowDown);
+	}
 
 }
 
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value.Get<FVector2D>().Y);
+	AddMovementInput(GetActorRightVector(), Value.Get<FVector2D>().X);
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+	AddControllerPitchInput(Value.Get<FVector2D>().Y * Sensitivity * GetWorld()->GetDeltaSeconds());
+	AddControllerYawInput(Value.Get<FVector2D>().X * Sensitivity * GetWorld()->GetDeltaSeconds());
+}
+
+void APlayerCharacter::PlayerJump(const FInputActionValue& Value)
+{
+	Jump();
+}
+
+void APlayerCharacter::PlayerCrouch(const FInputActionValue& Value)
+{
+	Crouch();
+}
+
+void APlayerCharacter::PlayerUnCrouch(const FInputActionValue& Value)
+{
+	UnCrouch();
+}
+
+void APlayerCharacter::Sprint(const FInputActionValue& Value)
+{
+	UCharacterMovementComponent* MovementComponent = Cast<UCharacterMovementComponent>(GetMovementComponent());
+	bRunning = true;
+	
+	if (AsthmaLimit > 0) {
+		MovementComponent->MaxWalkSpeed = 900;
+		AsthmaLimit -= GetWorld()->GetDeltaSeconds();
+	} else
+	{
+		MovementComponent->MaxWalkSpeed = 600;
+	}
+	
+}
+
+void APlayerCharacter::SlowDown(const FInputActionValue& Value)
+{
+	UCharacterMovementComponent* MovementComponent = Cast<UCharacterMovementComponent>(GetMovementComponent());
+	MovementComponent->MaxWalkSpeed *= 0;
+	MovementComponent->MaxWalkSpeed = 600;
+	bRunning = false;
+}
--- SPM-2026-whitebox/MyProject/Source/MyProject/PlayerCharacter.h
+++ SPM-2026-whitebox/MyProject/Source/MyProject/PlayerCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionvalue.h"
 #include "PlayerCharacter.generated.h"
 
 UCLASS()
@@ -18,6 +19,27 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	class UInputMappingContext* InputMappingCcntext;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	class UInputAction* IAMove;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* IAJump;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* IALook;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* IALookMouse;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* IACrouch;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* IASprint;
 
 public:	
 	// Called every frame
@@ -25,5 +47,23 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+private:
+	
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void PlayerJump(const FInputActionValue& Value);
+	void PlayerCrouch(const FInputActionValue& Value);
+	void PlayerUnCrouch(const FInputActionValue& Value);
+	void Sprint(const FInputActionValue& Value);
+	void SlowDown(const FInputActionValue& Value);
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	float Sensitivity = 50;
+	
+	UPROPERTY(EditAnywhere, Category="Input", BlueprintReadWrite, meta=(AllowPrivateAccess="true"))
+	float AsthmaLimit = 5;
+	
+	FTimerHandle TimerHandle;
+	
+	bool bRunning = false;
 };
